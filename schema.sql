1f05ac66c531050e55a2d11ded82e3bcf8888127
Liam <106878018+0roshi47@users.noreply.github.com>
2026-03-04 15:52:18 +0100

    page de connexion et de création de compte

--- schema.sql
+++ schema.sql
@@ -6,10 +6,26 @@ FLUSH PRIVILEGES;
 
 drop table if exists Message;
 
-create TABLE Message (
-    idMessage int not null auto_increment,
-    content VARCHAR(1000) not null,
-    author VARCHAR(32) not null,
-    postDate DATETIME not null,
-    constraint pk_message PRIMARY KEY(idMessage)
-)
+CREATE TABLE Author (
+   authorId int auto_increment,
+   password VARCHAR(50) not null,
+   login VARCHAR(50) not null,
+   PRIMARY KEY(authorId)
+);
+
+CREATE TABLE Room (
+   roomId int auto_increment,
+   roomName VARCHAR(50) not null,
+   PRIMARY KEY(roomId)
+);
+
+CREATE TABLE Message (
+   messageId int auto_increment,
+   content VARCHAR(500) not null,
+   postDate DATETIME not null,
+   authorId INT not null,
+   roomId INT not null,
+   PRIMARY KEY(messageId),
+   FOREIGN KEY(authorId) REFERENCES Author(authorId),
+   FOREIGN KEY(roomId) REFERENCES Room(roomId)
+);
